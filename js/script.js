4c1c60fdc8435f9a80b0efec17defe935646e39e
martinaliporata <martinaliporata@gmail.com>
2024-05-22 07:35:07 +0200

    aggiunto timer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,20 @@ const button = document.getElementById('button');
 
 button.addEventListener ('click', function() {
     // Step 1: Generare e visualizzare 5 numeri casuali
-    const randomNumbers = generateRandomNumbers(5, 1, 100);
-    displayNumbers(randomNumbers);
+    function getRandomInt(min,max) {
+        const randomNumbers = Math.floor(Math.random() * (max - min +1) + min);
+        return randomNumbers;
+    }
+    showedNumbers(randomNumbers);
+
+    if (userChoice > 10 || Number.isNaN(userChoice)){
+        userChoice = 3;
+    }
+
+    const timer = setInterval (function(){
+        userChoice--;
+        if (userChoice < 0)
+            clearInterval(timer);
+            alert('Now choose');
+    }, 30000)
 })
